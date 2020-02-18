d6338007dd9e2b50a0a62d90dfcc7e901a3f11b8
timo <tmakhlay2@gmail.com>
2020-02-17 20:44:06 -0800

    Made accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,6 @@ from accounts import views
 app_name = 'accounts'
 # Be careful setting the name to just /login use userlogin instead!
 urlpatterns=[
-    url(r'^register/$',views.register,name='register'),
-    url(r'^user_login/$',views.user_login,name='user_login'),
+    url(r'^register/$', views.register,name='register'),
+    url(r'^user_login/$', views.user_login,name='user_login'),
 ]
