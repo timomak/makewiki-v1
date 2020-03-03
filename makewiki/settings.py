2708b4f6cde990fd5587ff2d57991fd952061185
timo <tmakhlay2@gmail.com>
2020-03-02 16:08:05 -0800

    more tweaks

--- makewiki/settings.py
+++ makewiki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1yct-t!2bnkgc7j59z+9cdd2k)@y+ftqor$!aya()3if^cnlo-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'uniqueprojectname.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'uniqueprojectname.herokuapp.com', '127.0.0.1']
 
 # Login URL
 LOGIN_URL = 'accounts/login/'
@@ -132,7 +132,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
 
 # wiki app settings
 WIKI_PAGE_TITLE_MAX_LENGTH = 600
